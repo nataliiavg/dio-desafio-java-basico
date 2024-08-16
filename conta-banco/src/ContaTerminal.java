39aba792c0d2ad4ed1cf5d13d3bb1664cefdbeca
Natalia Graciano <nataliavgraciano@gmail.com>
2024-08-16 14:23:34 -0300

    Add solicitaçao de entrada e mensagem de saída dos dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = Integer.parseInt(leitura.nextLine());
+
+        System.out.println("Informe o número da agência: ");
+        agencia = leitura.nextLine();
+
+        System.out.println("Informe o nome do cliente: ");
+        nomeCliente = leitura.nextLine();
+
+        System.out.println("Informe o saldo da conta: ");
+        saldo = Double.parseDouble(leitura.nextLine());
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+    
+
+
     }
 }
